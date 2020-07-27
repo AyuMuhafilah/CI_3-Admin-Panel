7f531f85cc379349f9de34ec0a7c983ae4276846
KNobHack <akudandia7272@gmail.com>
2020-07-27 13:51:49 +0700

    more comtroller & package.json

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,16 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-
-        // Load model
-        $this->load->model('M_menu');
-        $this->load->model('M_menu_role');
-        $this->load->model('M_user');
-    }
-
     public function index()
     {
         $this->satpam->jaga(); // Penjaga Keamanan
