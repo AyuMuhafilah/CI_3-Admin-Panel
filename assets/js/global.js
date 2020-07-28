9db60093d7e46f31ca6083cbbb829bd86d1debf7
KNobHack <akudandia7272@gmail.com>
2020-07-28 10:56:47 +0700

    update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -10,7 +10,14 @@ function initial() {
 	$('.load-to-content').on('click', function (event) {
 		event.preventDefault();
 		let url = $(this).attr('href');
-		loadcontent(url, 'get');
+		loadContent(url, 'get');
+	});
+
+	// Ketika form di submit
+	$('form.form-load-to-content').on('submit', function (event) {
+		event.preventDefault();
+		let url = $(this).attr('action');
+		loadContent(url, 'post', new FormData(this));
 	});
 }
 
@@ -20,18 +27,17 @@ function initial() {
  * @param {string} url URL yang di targetkan
  * @param {string} method GET|HEAD|POST|PUT|PATCH|DELETE
  */
-function loadcontent(url, method, data = {}) {
-	$.ajax({
-		url: url,
-		method: method,
-		data: data,
-		async: false,
-		success: response => {
-			$('#content').html(response);
-			initial(); // Dilakukan inisialisasi kembali setelah content di load
-		},
-		error: error => {
-			console.log(error);
+function loadContent(url, method, data = {}) {
+	var xhr = new XMLHttpRequest(); // Ajax
+	xhr.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) { // Ketika status 200 (success)
+			$('#content').html(this.responseText);
+			initial();
+		} else { // Ketika gagal (error)
+			console.log(this);
 		}
-	});
+	};
+	xhr.open(method, url, false);
+	xhr.setRequestHeader("X-Requested-With", 'XMLHttpRequest'); // Memberitahu server bahwa request di kirim melalui Ajax
+	xhr.send(data);
 }
