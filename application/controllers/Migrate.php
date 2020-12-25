b7bb249b01b180b23ade524fe2d7e9c97fa781ed
KNobHack <akudandia7272@gmail.com>
2020-12-26 01:21:25 +0700

    Update Migration class

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -11,112 +11,97 @@ class Migrate extends CI_Controller
     /**
      * Nama database
      */
-    private $database = 'db_auth';
+    private $database;
 
     /**
      * Fungsi yang pertamakali di panggil
      */
     public function __construct()
     {
+        // Khusus dijalankan lewat CLI (tidak boleh lewat HTTP)
+        if (!is_cli()) show_404();
+
         parent::__construct();
-        // Khusus dijalankan lewat CLI (tidak boleh lewat HTML)
-        // dan dinonaktifkan ketika production
-        if (!is_cli() || (ENVIRONMENT === 'production')) {
-            show_404();
-            exit;
-        }
+        $this->load->library('migration');
 
-        $this->load->dbforge();
-
-        //  Load model
-        $this->load->model('M_example');
-        $this->load->model('M_foo');
-        $this->load->model('M_role');
-        $this->load->model('M_user');
-        $this->load->model('M_menu');
-        $this->load->model('M_menu_role');
+        $this->database = env('DB_NAME', 'db_auth');
     }
 
     /**
      * Menjalankan migrasi
-     */
-    public function run()
-    {
-        $this->db->query('use ' . $this->database);
-
-        // ==== DDL ====
-        $this->M_foo->migrate();
-        $this->M_example->migrate();
-        $this->M_role->migrate();
-        $this->M_user->migrate();
-        $this->M_menu->migrate();
-        $this->M_menu_role->migrate();
-
-        // ==== DML ====
-        $this->M_foo->seed();
-        $this->M_role->seed();
-        $this->M_user->seed();
-        $this->M_menu->seed();
-        $this->M_menu_role->seed();
-
-        // ==== DUMMY ====
-        $this->M_example->seed();
-    }
-
-    /**
-     * Membersihkan database
      * 
-     * Rollback ke database kosong
+     * @param int $version versi yang ingin di jalankan
      */
-    public function clear()
+    public function run(int $version = null)
     {
-        if ($this->dbforge->drop_database($this->database)) echo "\033[1;32mDatabase {$this->database} di hapus\033[0m\n";
-        if ($this->dbforge->create_database($this->database)) echo "\033[0;32mDatabse {$this->database} dibuat\033[0m\n";
+        // Jangan reset database jika sedang dalam fase production
+        // if (ENVIRONMENT === 'production' && $version === 0) show_error('Sedang dalam fase production!!! tidak bisa me-reset database');
+
+        if ($version === null)
+            $return_value = $this->migration->latest();
+        else
+            $return_value = $this->migration->version($version);
+
+        if ($return_value === false)
+            show_error($this->migration->error_string());
+        elseif ($return_value === true)
+            echo "\033[0;34mMigrasi sudah ada versi terbaru\033[0m\n";
+        elseif ($return_value === '0')
+            echo "\033[0;31mDatabse di reset\033[0m\n";
+        else
+            echo "\033[0;34mMigrasi di update ke versi {$return_value}\033[0m\n";
     }
 
     /**
-     * Rollback lalu jalankan migrasi
+     * Reset database seperti semula
+     * 
+     * @param string $hard hard
+     */
+    public function reset(string $hard = null)
+    {
+        // Jangan reset database jika sedang dalam fase production
+        // if (ENVIRONMENT === 'production') show_error('Sedang dalam fase production!!! tidak bisa me-reset database');
+
+        if (strtolower($hard) === 'hard') {
+            // Hapus database lalu buat lagi
+            if ($this->dbforge->drop_database($this->database)) echo "\033[0;31mDatabase {$this->database} di hapus\033[0m\n";
+            if ($this->dbforge->create_database($this->database)) echo "\033[0;32mDatabse {$this->database} dibuat\033[0m\n";
+        } else {
+            // reset dari migrasi
+            $this->run(0);
+        }
+    }
+
+    /**
+     * Buat database dari awal
      */
     public function refresh()
     {
-        $this->clear();
+        $this->run(0);
         $this->run();
     }
 
+    /**
+     * Alternatif
+     */
     public function index()
     {
         $this->run();
     }
 
     /**
-     * Warna untuk Console
-     * 
-     * ==== Huruf ====
-     * black => 0;30,
-     * dark_gray => 1;30,
-     * red => 0;31,
-     * bold_red => 1;31,
-     * green => 0;32,
-     * bold_green => 1;32,
-     * brown => 0;33,
-     * yellow => 1;33,
-     * blue => 0;34,
-     * bold_blue => 1;34,
-     * purple => 0;35,
-     * bold_purple => 1;35,
-     * cyan => 0;36,
-     * bold_cyan => 1;36,
-     * white => 1;37,
-     * bold_gray => 0;37,
-     *
-     * ==== Background ====
-     * black => 40,
-     * red => 41,
-     * magenta => 45,
-     * yellow => 43,
-     * green => 42,
-     * blue => 44,
-     * cyan => 46,
-     * light_gray => 47,
+     * Lihat file file migrasi yang tersedia
      */
+    public function migrations()
+    {
+        $migrations = $this->migration->find_migrations();
+
+        if (empty($migrations)) {
+            echo "\033[0;35mBelum ada migrasi tersedia\033[0m\n";
+        } else {
+            echo "\033[0;36mBerikut versi-versi migrasi yang tersedia\033[0m\n";
+            foreach ($migrations as $migration)
+                echo "\033[0;36m- {$migration}\033[0m\n";
+        }
+    }
 }
